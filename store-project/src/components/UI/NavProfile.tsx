64067de35fa9369740b94862c0afd6387052d473
lizka <lizka4231@gmail.com>
2025-02-24 23:39:48 +0300

    added items for profile user window

--- store-project/src/components/UI/NavProfile.tsx
+++ store-project/src/components/UI/NavProfile.tsx
@@ -2,8 +2,6 @@ import React, { FC, useContext } from 'react';
 import './NavProfile.scss';
 import { AuthContext } from '../../context/AuthContext';
 
-
-
 const NavProfile: FC = () => {
     const currAuthContext = useContext(AuthContext);
     if (!currAuthContext) return null;
@@ -16,10 +14,10 @@ const NavProfile: FC = () => {
         <>
         {isAuthed ?
             <div className='profile--user'>
-                <p className="profile--guest__title">Имя Фамилия</p>
-                <ul className="profile--guest__list">
+                <p className="profile--user__title">Имя Фамилия</p>
+                <ul className="profile--user__list">
                     {authedItems.map((item, index) =>
-                        <li className="profile--guest__item" key={`${item}${index}`}>
+                        <li className="profile--user__item" key={`${item}${index}`}>
                             {item}
                         </li>
                     )}
