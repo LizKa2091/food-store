89c7bbc4373740acadec636d499e210e28a6f73f
lizka <lizka4231@gmail.com>
2025-02-25 23:46:06 +0300

    added profile guest window

--- store-project/src/components/UI/NavProfile.tsx
+++ store-project/src/components/UI/NavProfile.tsx
@@ -1,33 +1,43 @@
 import React, { FC, useContext } from 'react';
-import './NavProfile.scss';
 import { AuthContext } from '../../context/AuthContext';
+import './NavProfile.scss';
 
 const NavProfile: FC = () => {
-    const currAuthContext = useContext(AuthContext);
-    if (!currAuthContext) return null;
-
-    const {isAuthed} = currAuthContext; 
+    const currAuthContext = useContext(AuthContext) || { isAuthed: false };
+    const { isAuthed } = currAuthContext; 
 
     const authedItems: string[] = ['Профиль', 'Заказы', 'Бонусы', 'Избранное', 'Выход'];
 
     return (
         <>
-        {isAuthed ?
-            <div className='profile--user'>
-                <p className="profile--user__title">Имя Фамилия</p>
-                <ul className="profile--user__list">
-                    {authedItems.map((item, index) =>
-                        <li className="profile--user__item" key={`${item}${index}`}>
-                            {item}
+            {!isAuthed ? (
+                <div className='profile profile--user'>
+                    <p className="profile--user__title">Имя Фамилия</p>
+                    <ul className="profile--user__list">
+                        {authedItems.map((item, index) =>
+                            <li className="profile--user__item" key={`${item}${index}`}>
+                                {item}
+                            </li>
+                        )}
+                    </ul>
+                </div>
+            ) : (
+                <div className='profile profile--guest guest__step1'>
+                    <p className="profile--guest__title">Авторизуйтесь</p>
+                    <ul className="profile--guest__list">
+                        <li className="profile--guest__item profile--guest__item--1">
+                            Покупайте продукты со скидкой
                         </li>
-                    )}
-                </ul>
-            </div>
-            :
-            <div className='profile--guest'>
-
-            </div>
-        }
+                        <li className="profile--guest__item profile--guest__item--2">
+                            Заказывайте товары с доставкой день в день
+                        </li>
+                        <li className="profile--guest__item profile--guest__item--3">
+                            Узнавайте первыми и участвуйте в акциях
+                        </li>
+                    </ul>
+                    <button className="profile--guest__button">Войти по номеру телефона</button>
+                </div>
+            )}
         </>
     )
 }
