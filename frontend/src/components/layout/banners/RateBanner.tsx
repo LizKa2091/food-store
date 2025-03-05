340a9fe3ebbcb98e8915dffd3c8dddcad881474d
lizka <lizka4231@gmail.com>
2025-03-05 16:09:05 +0300

    added rate banner component

--- frontend/src/components/layout/banners/RateBanner.tsx
+++ frontend/src/components/layout/banners/RateBanner.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import './RateBanner.scss';
+import star from '../../../images/star.png';
+import emptyStar from '../../../images/empty-star.png';
 
 const RateBanner = () => {
   return (
-    <div></div>
+    <div className='rate-banner'>
+      <div className="rate-banner__col">
+        <p className="rate-banner__title">Оцените магазин</p>
+        <p className="rate-banner__subtitle">Поделитесь впечатлениями о заказе и помогите сделать нас лучше</p>
+      </div>
+      <div className="rate-banner__col rate-banner__col--stars">
+        <div className="rate-banner__container--stars">
+            <img src={star} className='star' alt="star" />
+            <img src={star} className='star' alt="star" />
+            <img src={star} className='star' alt="star" />
+            <img src={star} className='star' alt="star" />
+            <img src={emptyStar} className='star' alt="empty star" />
+        </div>
+        <button className="rate-banner__button">Оставить отзыв</button>
+      </div>
+  </div>
   )
 };
 
