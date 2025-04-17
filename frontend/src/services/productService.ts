c2f4bc250f9b9587da4e0e87c6467d2265134a4b
Liza <lizka4231@gmail.com>
2025-04-17 15:15:33 +0300

    add request for frontend for products by category

--- frontend/src/services/productService.ts
+++ frontend/src/services/productService.ts
@@ -1,7 +1,9 @@
+const baseUrl = 'http://localhost:5001';
+
 const addToFavorites = async (token: string, productId: string) => {
     let response;
     try {
-        response = await fetch('http://localhost:5001/favorites/add', {
+        response = await fetch(`${baseUrl}/favorites/add`, {
             method: 'POST',
             headers: {
                 'Authorization': token,
@@ -27,7 +29,7 @@ const addToFavorites = async (token: string, productId: string) => {
 const removeFromFavorites = async (token: string, productId: string) => {
     let response;
     try {
-        response = await fetch('http://localhost:5001/favorites/remove', {
+        response = await fetch(`${baseUrl}/favorites/remove`, {
             method: 'DELETE',
             headers: {
                 'Authorization': token,
@@ -53,7 +55,7 @@ const removeFromFavorites = async (token: string, productId: string) => {
 const getProduct = async (id: string) => {
     let response;
     try {
-       response = await fetch(`http://localhost:5001/products/${id}`, {
+       response = await fetch(`${baseUrl}/products/${id}`, {
            method: 'GET',
            headers: {
               'Content-Type': 'application/json'
@@ -74,4 +76,30 @@ const getProduct = async (id: string) => {
   }
 };
 
-export { addToFavorites, removeFromFavorites, getProduct };
+const getCatalogProducts = async (category: string) => {
+    let response;
+
+    try {
+        response = await fetch(`${baseUrl}/products-by-category`, {
+            method: 'POST',
+            headers: {
+               'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ catalogCategory: category })
+        });
+ 
+        if (!response.ok) {
+            const errorData = await response.json();
+            return errorData || 'Неизвестная ошибка';
+        }
+ 
+        let result = await response.json();
+        return result;
+    }
+    catch (e) {
+        const errorData = await response?.json();
+        return errorData || 'Неизвестная ошибка'
+   }
+}
+
+export { addToFavorites, removeFromFavorites, getProduct, getCatalogProducts };
