76d6de528f6341b355cac80953b71b8988b69124
lizka <lizka4231@gmail.com>
2025-04-23 18:17:12 +0300

    refactor authcontext

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,23 +1,54 @@
 import React, { createContext, useState, FC, ReactNode } from 'react';
+import { logout, postCode } from '../services/authService';
 
 interface IAuthContext {
     isAuthed: boolean;
-    loginUser: () => void;
-    logoutUser: () => void;
+    loginUser: (phoneNumber: string, code: string) => Promise<void>;
+    logoutUser: () => Promise<void>;
 };
 
 const AuthContext = createContext<IAuthContext | undefined>(undefined);
 
 const AuthProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const [isAuthed, setIsAuthed] = useState<boolean>(false);
-    const loginUser = () => setIsAuthed(true);
-    const logoutUser = () => setIsAuthed(false);
+   const [isAuthed, setIsAuthed] = useState<boolean>(false);
 
-    return (
-        <AuthContext.Provider value={{ isAuthed, loginUser, logoutUser }}>
-            {children}
-        </AuthContext.Provider>
-    );
+   const loginUser = async (phoneNumber: string, code: string) => {
+      let result;
+      try {
+         result = await postCode(phoneNumber, code);
+
+         if (result.token) {
+            localStorage.setItem('token', result.token);
+            setIsAuthed(true);
+         }
+      }
+      catch (e) {
+         console.error(e);
+         return e;
+      }
+      return result;
+   };
+
+   const logoutUser = async () => {
+      try {
+         const token = localStorage.getItem('token');
+         if (token) {
+            await logout(token);
+            localStorage.removeItem('token');
+            setIsAuthed(false);
+         }
+      } 
+      catch (error) {
+         console.error(error);
+         throw new Error('Не удалось выйти из системы.');
+      }
+   };
+
+   return (
+      <AuthContext.Provider value={{ isAuthed, loginUser, logoutUser }}>
+         {children}
+      </AuthContext.Provider>
+   );
 };
 
 export { AuthContext, AuthProvider };
