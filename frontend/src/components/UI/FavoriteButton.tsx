3bb46d6d27de82f40ed196292e4b4229b99d44ef
lizka <lizka4231@gmail.com>
2025-04-23 19:04:47 +0300

    remove favorite button propagation

--- frontend/src/components/UI/FavoriteButton.tsx
+++ frontend/src/components/UI/FavoriteButton.tsx
@@ -5,68 +5,69 @@ import { AuthContext } from '../../context/AuthContext';
 import './FavoriteButton.scss';
 
 interface IFavoriteButtonProps {
-    productId: string;
-    initialFavState: boolean;
-    position?: string;
+   productId: string;
+   initialFavState: boolean;
+   position?: string;
 };
 
 const FavoriteButton: FC<IFavoriteButtonProps> = ({ productId, initialFavState, position }) => {
-    const [isFavorited, setIsFavorited] = useState<boolean>(initialFavState);
-    const { isAuthed } = useContext(AuthContext) || { isAuthed: false };
+   const [isFavorited, setIsFavorited] = useState<boolean>(initialFavState);
+   const { isAuthed } = useContext(AuthContext) || { isAuthed: false };
 
-    const { setMessage } = useMessage();
+   const { setMessage } = useMessage();
 
-    useEffect(() => {
-        setIsFavorited(initialFavState);
-    }, [initialFavState]);
+   useEffect(() => {
+      setIsFavorited(initialFavState);
+   }, [initialFavState]);
 
-    useEffect(() => {
-      if (!isAuthed) {
-         setIsFavorited(false);
-      }
-    }, [isAuthed]);
+   useEffect(() => {
+   if (!isAuthed) {
+      setIsFavorited(false);
+   }
+   }, [isAuthed]);
 
-    const handleClick = async (): Promise<void> => {
-        const token = localStorage.getItem('token');
+   const handleClick = async (e: React.MouseEvent<HTMLButtonElement>): Promise<void> => {
+      e.stopPropagation();
+      const token = localStorage.getItem('token');
 
-        if (token) {
-            let response;
-            try {   
-                if (isFavorited) {
-                    response = await removeFromFavorites(token, productId);
-                }
-                else {
-                    response = await addToFavorites(token, productId);
-                }
-
-                if (response.hasOwnProperty('favorites')) {
-                    setIsFavorited(prevVal => !prevVal);
-                }
-                else if (response.hasOwnProperty('warning')) {
-                  setMessage(response.warning);
-                }
-                else {
-                  setMessage(response.message);
-                }
+      if (token) {
+         let response;
+         try {   
+            if (isFavorited) {
+               response = await removeFromFavorites(token, productId);
             }
-            catch (e) {
+            else {
+               response = await addToFavorites(token, productId);
+            }
+
+            if (response.hasOwnProperty('favorites')) {
+               setIsFavorited(prevVal => !prevVal);
+            }
+            else if (response.hasOwnProperty('warning')) {
+               setMessage(response.warning);
+            }
+            else {
                setMessage(response.message);
             }
-        }
-        else {
-         setMessage('Пожалуйста, авторизуйтесь');
-        }
-    };
+         }
+         catch (e) {
+            setMessage(response.message);
+         }
+      }
+      else {
+      setMessage('Пожалуйста, авторизуйтесь');
+      }
+   };
 
-    return (
-        <>
-            <button 
-                className={'favorite-button' + (isFavorited ? ' favorite-button--favorited' : '') + (position === 'relative' ? ' favorite-button--relative' : '')} 
-                onClick={ handleClick }
-            >
-            </button>
-        </>
-    )
+   return (
+      <>
+         <button 
+            className={'favorite-button' + (isFavorited ? ' favorite-button--favorited' : '') + (position === 'relative' ? ' favorite-button--relative' : '')} 
+            onClick={ handleClick }
+         >
+         </button>
+      </>
+   )
 };
 
 export default FavoriteButton;
--- frontend/src/components/UI/SalesAndRecommendation.tsx
+++ frontend/src/components/UI/SalesAndRecommendation.tsx
@@ -10,113 +10,113 @@ import './SalesAndRecommendation.scss';
 type CategoryType = 'Скидки' | 'Рекомендации для вас';
 
 interface ICategoryProps {
-    type: CategoryType;
-    onModalChange?: (modalState: boolean) => void;
+   type: CategoryType;
+   onModalChange?: (modalState: boolean) => void;
 };
 
 interface FavoriteItem {
-    productId: string;
-    name: string;
-    price: number;
-    stockQuantity: number;
-    weight: string;
-    newPrice?: number;
-    imagePath?: string;
+   productId: string;
+   name: string;
+   price: number;
+   stockQuantity: number;
+   weight: string;
+   newPrice?: number;
+   imagePath?: string;
 };
 
 interface Item {
-    productId: string;
-    image: string;
-    amount: number;
-    name: string;
-    price: number;
-    oldPrice?: number;
+   productId: string;
+   image: string;
+   amount: number;
+   name: string;
+   price: number;
+   oldPrice?: number;
 };
 
 const SalesAndRecommendation = ({ type, onModalChange } : ICategoryProps) => {
-    const [userFavorites, setUserFavorites] = useState<string[] | null>(null);
-    const [isItemClicked, setIsItemClicked] = useState<boolean>(false);
-    const [selectedProductId, setSelectedProductId] = useState<string | null>(null);
+   const [userFavorites, setUserFavorites] = useState<string[] | null>(null);
+   const [isItemClicked, setIsItemClicked] = useState<boolean>(false);
+   const [selectedProductId, setSelectedProductId] = useState<string | null>(null);
 
-    const { setMessage } = useMessage();
+   const { setMessage } = useMessage();
 
-    useEffect(() => {
-        getUserFavorites();
-    }, []);
+   useEffect(() => {
+      getUserFavorites();
+   }, []);
 
-    const items: Record<string, Item> = {
-        item1: { productId: '1', image: item1Image, amount: 2, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 99.90, oldPrice: 129.00},
-        item2: { productId: '2', image: item2Image, amount: 33, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 70.90},
-        item3: { productId: '3', image: item1Image, amount: 0, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 99.90, oldPrice: 129.00},
-    };
+   const items: Record<string, Item> = {
+      item1: { productId: '1', image: item1Image, amount: 2, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 99.90, oldPrice: 129.00},
+      item2: { productId: '2', image: item2Image, amount: 33, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 70.90},
+      item3: { productId: '3', image: item1Image, amount: 0, name: 'Гранола Мюсли Bionova ягодные запечённые хрустящие, 400г', price: 99.90, oldPrice: 129.00},
+   };
 
-    const getUserFavorites = async () => {
-        const token = localStorage.getItem('token');
+   const getUserFavorites = async () => {
+      const token = localStorage.getItem('token');
 
-        if (token) {
-            let response;
-            try {
-                response = await fetchUserFavorites(token);
+      if (token) {
+         let response;
+         try {
+            response = await fetchUserFavorites(token);
 
-                const favorites = response.favorites.map((item: FavoriteItem) => item.productId);
-                setUserFavorites(favorites);
-                setMessage('');
-            }
-            catch(e) {
-                setMessage(response?.message);
-            }
-        }
-    };
+            const favorites = response.favorites.map((item: FavoriteItem) => item.productId);
+            setUserFavorites(favorites);
+            setMessage('');
+         }
+         catch(e) {
+            setMessage(response?.message);
+         }
+      }
+   };
 
-    const handleItemClick = (productId: string) => {
-        setSelectedProductId(productId);
-        setIsItemClicked(true);
-        onModalChange?.(true)
-    };  
+   const handleItemClick = (productId: string) => {
+      setSelectedProductId(productId);
+      setIsItemClicked(true);
+      onModalChange?.(true)
+   };  
 
-    const handleItemAction = (newState: boolean) => {
-        setIsItemClicked(newState);
-        onModalChange?.(false)
-    };
+   const handleItemAction = (newState: boolean) => {
+      setIsItemClicked(newState);
+      onModalChange?.(false)
+   };
 
-    return (
-        <>
-            <section className="sales-and-recommendation">
-                <div className="sales-and-recommendation__top">
-                    <p className="sales-and-recommendation__title">{type}</p>
-                    <button className="sales-and-recommendation__button">Смотреть все</button>
-                </div>
-                <div className="sales-and-recommendation__bottom">
-                    <ul className="sales-and-recommendation__list">
-                        {Object.entries(items).map(([key, item]) => (
-                            <li onClick={ () => handleItemClick(item.productId) } key={key} className={item.oldPrice ? "sales-and-recommendation__item item--onsale" : "sales-and-recommendation__item"}>
-                                <FavoriteButton productId={item.productId} initialFavState={userFavorites ? userFavorites.includes(item.productId) : false} />
-                                <img src={item.image} className="sales-and-recommendation__item__img" alt={item.name}/>
-                                <div className="sales-and-recommendation__item__left">
-                                    {item.amount !== 0 ?
-                                        <p className="sales-and-recommendation__item__amount">В наличии {item.amount}</p>
-                                        : <p className="sales-and-recommendation__item__amount item--outofstock">Появится завтра</p>
-                                    }
-                                    <p className="sales-and-recommendation__item__title">{item.name}</p>
-                                    <p className="sales-and-recommendation__item__price">{item.price} руб</p>
-                                    {item.oldPrice ?
-                                        <p className="sales-and-recommendation__item__price--old">{item.oldPrice} руб</p>
-                                        : ''
-                                    }
-                                </div>
-                                <div className="sales-and-recommendation__item__right">
-                                    <button className="sales-and-recommendation__item__button">{item.amount !== 0 ? "В корзину" : "На завтра"}</button>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </section>
-            {isItemClicked && selectedProductId &&
-                <ItemCard onModalAction={ handleItemAction } id={ selectedProductId }/>
-            }
-        </>
-    );
+   return (
+      <>
+         <section className="sales-and-recommendation">
+               <div className="sales-and-recommendation__top">
+                  <p className="sales-and-recommendation__title">{type}</p>
+                  <button className="sales-and-recommendation__button">Смотреть все</button>
+               </div>
+               <div className="sales-and-recommendation__bottom">
+                  <ul className="sales-and-recommendation__list">
+                     {Object.entries(items).map(([key, item]) => (
+                        <li onClick={ () => handleItemClick(item.productId) } key={key} className={item.oldPrice ? "sales-and-recommendation__item item--onsale" : "sales-and-recommendation__item"}>
+                           <FavoriteButton productId={item.productId} initialFavState={userFavorites ? userFavorites.includes(item.productId) : false} />
+                           <img src={item.image} className="sales-and-recommendation__item__img" alt={item.name}/>
+                           <div className="sales-and-recommendation__item__left">
+                              {item.amount !== 0 ?
+                                 <p className="sales-and-recommendation__item__amount">В наличии {item.amount}</p>
+                                 : <p className="sales-and-recommendation__item__amount item--outofstock">Появится завтра</p>
+                              }
+                              <p className="sales-and-recommendation__item__title">{item.name}</p>
+                              <p className="sales-and-recommendation__item__price">{item.price} руб</p>
+                              {item.oldPrice ?
+                                 <p className="sales-and-recommendation__item__price--old">{item.oldPrice} руб</p>
+                                 : ''
+                              }
+                           </div>
+                           <div className="sales-and-recommendation__item__right">
+                              <button className="sales-and-recommendation__item__button">{item.amount !== 0 ? "В корзину" : "На завтра"}</button>
+                           </div>
+                        </li>
+                     ))}
+                  </ul>
+               </div>
+         </section>
+         {isItemClicked && selectedProductId &&
+            <ItemCard onModalAction={ handleItemAction } id={ selectedProductId }/>
+         }
+      </>
+   );
 };
 
 export default SalesAndRecommendation;
