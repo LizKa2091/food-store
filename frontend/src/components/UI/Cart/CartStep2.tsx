201f6b08617c7c9a0446549377bbefc97c2b9ad0
lizka <lizka4231@gmail.com>
2025-04-24 23:59:42 +0300

    add cart step 2

--- frontend/src/components/UI/Cart/CartStep2.tsx
+++ frontend/src/components/UI/Cart/CartStep2.tsx
@@ -7,9 +7,40 @@ interface ICartStep2Props {
 
 const CartStep2: FC<ICartStep2Props> = ({ children }) => {
    return (
-      <div>
-         step2
-      </div>
+      <main className='main'>
+         <div className="main__left">
+            <form className="main__form--order">
+               <h2 className="main__title">Оформление заказа</h2>
+               <div className="main__details">
+                  <h3 className="main__subtitle">Ваши данные</h3>
+                  <div className="main__details-row">
+                     <p className="main__details-fio">Имя Фамилия</p>
+                     <button className="main__details-button main__details-button--fio">Изменить получателя</button>
+                  </div>
+                  <div className="main__details-row">
+                     <p className="main__details-phone">+38 999 999 99 99</p>
+                     <button className="main__details-button main__details-button--phone">Изменить контактный номер для заказа</button>
+                  </div>
+                  <p className="main__details-card">К этому номеру телефона привязана карта №a437503</p>
+                  <h3 className="main__subtitle">Способ оплаты</h3>
+                  <div className="main__radios">
+                     <div className="main__radio-row">
+                        <input type="radio" name="" id="" className='main__radio-input' />
+                        <label htmlFor="" className="main__radio-label"></label>
+                     </div>
+                     <div className="main__radio-row">
+                        <input type="radio" name="" id="" className='main__radio-input' />
+                        <label htmlFor="" className="main__radio-label"></label>
+                     </div>
+                     <div className="main__radio-row">
+                        <input type="radio" name="" id="" className='main__radio-input' />
+                        <label htmlFor="" className="main__radio-label"></label>
+                     </div>
+                  </div>
+               </div>
+            </form>
+         </div>
+      </main>
    )
 };
 
