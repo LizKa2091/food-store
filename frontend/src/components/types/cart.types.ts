25479c901da9962ee9c357f3d641d4ad708a7423
lizka <lizka4231@gmail.com>
2025-04-28 22:38:32 +0300

    add types for cart

---
+++ frontend/src/components/types/cart.types.ts
@@ -0,0 +1,32 @@
+import bakeryImg1 from '../images/webpImages/catalogItems/catalog-item-1.webp';
+import bakeryImg2 from '../images/webpImages/catalogItems/catalog-item-2.webp';
+import bakeryImg3 from '../images/webpImages/catalogItems/catalog-item-3.webp';
+import bakeryImg4 from '../images/webpImages/catalogItems/catalog-item-4.webp';
+import bakeryImg5 from '../images/webpImages/catalogItems/catalog-item-5.webp';
+import bakeryImg6 from '../images/webpImages/catalogItems/catalog-item-6.webp';
+
+export interface ICartItem {
+   productId: string;
+   name: string;
+   price: number;
+   stockQuantity: number;
+   weight: string;
+   newPrice?: number;
+   imagePath: ImageKeys;
+   filterCategory: string;
+};
+
+export type CartValues = {
+   cart: ICartItem[];
+};
+
+export type ImageKeys = keyof typeof images;
+
+export const images = {
+   bakeryImg1,
+   bakeryImg2,
+   bakeryImg3,
+   bakeryImg4,
+   bakeryImg5,
+   bakeryImg6,
+};
