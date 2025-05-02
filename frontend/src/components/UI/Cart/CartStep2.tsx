aae4f4db28f086d37553a6baa946a8fe310a3cfb
lizka <lizka4231@gmail.com>
2025-05-02 21:04:22 +0300

    add main logic for cart step 2

--- frontend/src/components/UI/Cart/CartStep2.tsx
+++ frontend/src/components/UI/Cart/CartStep2.tsx
@@ -1,4 +1,7 @@
-import React, { FC, ReactNode } from 'react';
+import React, { ChangeEvent, MouseEvent, FC, FormEvent, ReactNode, useEffect, useState } from 'react';
+import { useMessage } from '../../../context/MessageContext';
+import { fetchBonusCard, fetchUserInfo, updateUserInfo } from '../../../services/userService';
+import { IUserBonuses, IUserInfo } from '../../../types/userData.types';
 import './CartStep2.scss';
 
 interface ICartStep2Props {
@@ -6,6 +9,120 @@ interface ICartStep2Props {
 };
 
 const CartStep2: FC<ICartStep2Props> = ({ children }) => {
+   const [userInfo, setUserInfo] = useState<IUserInfo>({ nameSurname: '', phoneNumber: '', dateOfBirth: '', email: '' });
+   const [userBonusCard, setUserBonusCard] = useState<IUserBonuses>({ bonuses: '', cardNumber: '' });
+   const [isInputWrong, setIsInputWrong] = useState<boolean>(false);
+   const [isFormSaved, setIsFormSaved] = useState<boolean | null>(null);
+   const [isDirty, setIsDirty] = useState<boolean>(false);
+
+   const { setMessage } = useMessage();
+
+   useEffect(() => {
+      loadUserInfo();
+      loadUserCard();
+   }, []);
+
+   const loadUserInfo = async () => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+         let response;
+
+         try {
+            response = await fetchUserInfo(token);
+
+            setUserInfo(response.user)
+         }
+         catch (e) {
+            setMessage(response.message);
+         }
+      }
+      else {
+         setMessage('пользователь не авторизован');
+      }
+   };
+
+   const loadUserCard = async () => {
+      const token = localStorage.getItem('token');
+
+      if (token) {
+         let response;
+
+         try {
+            response = await fetchBonusCard(token);
+
+            setUserBonusCard(response.bonusCard);
+         }
+         catch (e) {
+            setMessage(response.message);
+         }
+      }
+      else {
+         setMessage('пользователь не авторизован');
+      }
+   };
+
+   const accessValueChange = (valueType: string, e: MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+   };
+
+   const handleNameChange = (e: ChangeEvent<HTMLInputElement>): void => {
+      setUserInfo((prev) => ({
+         ...prev,
+         nameSurname: e.target.value
+      }));
+      setIsDirty(true);
+
+      const nameRegex = /^[A-ZА-Я][a-zа-яё]+ [A-ZА-Я][a-zа-яё]+$/;
+
+      if (!nameRegex.test(e.target.value)) {
+         e.target.classList.add('wrong-input');
+         setIsInputWrong(true);
+      }
+      else {
+         e.target.classList.remove('wrong-input');
+         setIsInputWrong(false);
+      }
+   };
+
+   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (!isInputWrong && isDirty) {
+         const token = localStorage.getItem('token');
+
+         if (token) {
+            const { nameSurname, phoneNumber, dateOfBirth, email } = userInfo;
+            let response;
+
+            try {
+               response = await updateUserInfo(token, nameSurname, phoneNumber, dateOfBirth, email);
+
+               if (response.user) {
+                  setIsFormSaved(true);
+                  setIsDirty(false);
+               }
+            }
+            catch (e) {
+               setMessage(response.message);
+            }
+         }
+         else {
+            setMessage('Пользователь не авторизован');
+         }
+      }
+      else if (isDirty) {
+         setIsFormSaved(false);
+      }
+      else {
+         setIsFormSaved(!isInputWrong);
+      }
+   };
+
+   const formatPhoneNum = (num: string) => {
+      return `+${num.slice(0, 1)} ${num.slice(1, 4)} ${num.slice(4, 7)} ${num.slice(7, 9)} ${num.slice(9, 11)}`;
+   };
+
    return (
       <main className='main'>
          <div className="main__left">
@@ -14,14 +131,14 @@ const CartStep2: FC<ICartStep2Props> = ({ children }) => {
                <div className="main__details">
                   <h3 className="main__subtitle">Ваши данные</h3>
                   <div className="main__details-row">
-                     <p className="main__details-fio">Имя Фамилия</p>
-                     <button className="main__details-button main__details-button--fio">Изменить получателя</button>
+                     <p className={"main__details-fio" + (userInfo.nameSurname.length === 0 ? ' main__details-fio--empty' : '')}>{userInfo.nameSurname.length > 0 ? userInfo.nameSurname : 'Не указано'}</p>
+                     <button onClick={(e: MouseEvent<HTMLButtonElement>) => accessValueChange('fio', e)} className="main__details-button main__details-button--fio">Изменить получателя</button>
                   </div>
                   <div className="main__details-row">
-                     <p className="main__details-phone">+38 999 999 99 99</p>
-                     <button className="main__details-button main__details-button--phone">Изменить контактный номер для заказа</button>
+                     <p className="main__details-phone">{formatPhoneNum(userInfo.phoneNumber)}</p>
+                     <button onClick={(e: MouseEvent<HTMLButtonElement>) => accessValueChange('phone', e)} className="main__details-button main__details-button--phone">Изменить контактный номер для заказа</button>
                   </div>
-                  <p className="main__details-card">К этому номеру телефона привязана карта №a437503</p>
+                  <p className="main__details-card">К этому номеру телефона привязана карта №{userBonusCard.cardNumber}</p>
                   <h3 className="main__subtitle">Способ оплаты</h3>
                   <div className="main__radios">
                      <div className="main__radio-row">
@@ -34,12 +151,11 @@ const CartStep2: FC<ICartStep2Props> = ({ children }) => {
                      </div>
                      <div className="main__radio-row">
                         <input type="radio" name="receipt-type" id="online" value='online' className='main__radio-input' />
-                        <label htmlFor="online" className="main__radio-label">Онлайн оплата</label>
+                        <label htmlFor="online" className="main__radio-label main__radio-label--last">Онлайн оплата</label>
                      </div>
                   </div>
                   <div className="main__details-row">
-                     <h3 className="main__subtitle">Способ оплаты</h3>
-                     <button className="main__details-button main__details-button--receiver">Изменить получателя</button>
+                     <h3 className="main__subtitle">Детали адреса</h3>
                   </div>
                   <p className="main__details-address">ул. Новая,Ильинское-Усово, городской округ Красногорск</p>
                   <div className="main__details-form-els">
