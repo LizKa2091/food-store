486d130c1c4f5abb72ae0b31a6baa529c1fdec15
lizka <lizka4231@gmail.com>
2025-05-19 00:47:06 +0300

    feat: add search bar

---
+++ frontend/src/components/layout/header/SearchBar.tsx
@@ -0,0 +1,29 @@
+import React, { ChangeEvent, FC, FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar: FC = () => {
+   const [searchInput, setSearchInput] = useState<string>('');
+
+   const navigate = useNavigate();
+
+   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+      setSearchInput(e.target.value);
+   };
+
+   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      if (searchInput.trim().length > 0) {
+         navigate(`/catalog/${searchInput}`);
+      }
+   };
+
+   return (
+      <form onSubmit={handleFormSubmit} className='nav__form'>
+         <input type="text" value={searchInput} onChange={handleInputChange} className="nav__input" id='nav-search' name='nav-search' />
+         <button className='nav__input-button' title='Поиск'></button>
+      </form>
+   )
+}
+
+export default SearchBar;
