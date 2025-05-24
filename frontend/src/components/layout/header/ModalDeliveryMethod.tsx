060bdea2981b92aa826a9a6873e6db6dbd51abd9
Liza <lizka4231@gmail.com>
2025-05-24 14:07:32 +0300

    feat: add modal delivery method compontent

---
+++ frontend/src/components/layout/header/ModalDeliveryMethod.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import './ModalDeliveryMethod.scss';
+
+const ModalDeliveryMethod: FC = () => {
+   return (
+      <div>
+         modal delivery method
+      </div>
+   )
+}
+
+export default ModalDeliveryMethod;
