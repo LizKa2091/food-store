1663463d98a376741a01c12c4b59274680681c1b
lizka <lizka4231@gmail.com>
2025-05-25 00:13:24 +0300

    feat: finish modal delivery method component

--- frontend/src/components/layout/header/ModalDeliveryMethod.tsx
+++ frontend/src/components/layout/header/ModalDeliveryMethod.tsx
@@ -24,9 +24,13 @@ const ModalDeliveryMethod: FC<IModalDeliveryMethod> = ({ onModalChange }) => {
                      бесплатно
                   </button>
                </div>
-               <label htmlFor="address" className='modal-delivery__form-label'>Выберите адрес доставки</label>
+               <label htmlFor="address" className='modal-delivery__form-label'>
+                  Выберите адрес {chosenMethod === 'delivery' ? 'доставки' : 'самовывоза'}
+               </label>
                <input type="text" id='address' className='modal-delivery__form-input' placeholder='Выберите адрес доставки' />
-               <label htmlFor="date" className='modal-delivery__form-label'>Когда доставить?</label>
+               <label htmlFor="date" className='modal-delivery__form-label'>
+                  Когда {chosenMethod === 'delivery' ? 'доставить' : 'зайдёте в магазин'}?
+               </label>
                <div className="modal-delivery-row">
                   <input className='modal-delivery__form-input modal-delivery__form-input--date' type="date" name="date" id="date" />
                   <select name="time" id="time" className='modal-delivery__form-select'>
@@ -35,6 +39,7 @@ const ModalDeliveryMethod: FC<IModalDeliveryMethod> = ({ onModalChange }) => {
                   </select>
                </div>
             </div>
+            <button className='modal-delivery__form-submit' onClick={ (e: MouseEvent<HTMLButtonElement>) => {e.preventDefault(); onModalChange(false)} } type="submit">Сохранить</button>
          </form>
          <iframe title='delivery-map' className='modal-delivery__iframe' src="https://yandex.ru/map-widget/v1/?ll=37.570311%2C55.729953&mode=search&poi%5Bpoint%5D=37.483804%2C55.748278&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1072958798&sll=37.460254%2C55.680326&sspn=0.009194%2C0.003029&text=%D0%BF%D0%B5%D1%80%D0%B5%D0%BA%D1%80%D1%91%D1%81%D1%82%D0%BE%D0%BA&z=12.3" />
          <button onClick={ (e: MouseEvent<HTMLButtonElement>) => {e.stopPropagation(); onModalChange(false)} } className="modal-delivery__close">x</button>
