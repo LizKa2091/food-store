c9c3956be788de66641f70a7074291f34594f6fc
lizka <lizka4231@gmail.com>
2025-05-26 23:11:55 +0300

    feat: add saving delivery info to localstorage

--- frontend/src/components/layout/header/ModalDeliveryMethod.tsx
+++ frontend/src/components/layout/header/ModalDeliveryMethod.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState, MouseEvent } from 'react';
+import React, { FC, useState, MouseEvent, useEffect, FormEvent, ChangeEvent } from 'react';
 import './ModalDeliveryMethod.scss';
 
 interface IModalDeliveryMethod {
@@ -9,10 +9,19 @@ type deliveryMethods = 'delivery' | 'takeout';
 
 const ModalDeliveryMethod: FC<IModalDeliveryMethod> = ({ onModalChange }) => {
    const [chosenMethod, setChosenMethod] = useState<deliveryMethods>('delivery');
+   const [address, setAddress] = useState<string>('');
+   const [deliveryDate, setDeliveryDate] = useState<string>('');
+   const [deliveryTime, setDeliveryTime] = useState<string>('for 25 mins');
+
+   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+
+      localStorage.setItem('deliveryInfo', JSON.stringify({ address, deliveryDate, deliveryTime }));
+   };
 
    return (
       <div className='modal-delivery-container'>
-         <form className='modal-delivery__form'>
+         <form onSubmit={handleSubmit} className='modal-delivery__form'>
             <div className="modal-delivery--left">
                <div className="modal-delivery-row">
                   <button onClick={ (e: MouseEvent<HTMLButtonElement>) => { e.preventDefault(); setChosenMethod('delivery')} } className={"modal-delivery__button" + (chosenMethod === 'delivery' ? ' active' : '')}>
@@ -27,22 +36,22 @@ const ModalDeliveryMethod: FC<IModalDeliveryMethod> = ({ onModalChange }) => {
                <label htmlFor="address" className='modal-delivery__form-label'>
                   Выберите адрес {chosenMethod === 'delivery' ? 'доставки' : 'самовывоза'}
                </label>
-               <input type="text" id='address' className='modal-delivery__form-input' placeholder='Выберите адрес доставки' />
+               <input value={address} onChange={(e: ChangeEvent<HTMLInputElement>) => setAddress(e.target.value)} type="text" id='address' className='modal-delivery__form-input' placeholder='Выберите адрес доставки' required/>
                <label htmlFor="date" className='modal-delivery__form-label'>
                   Когда {chosenMethod === 'delivery' ? 'доставить' : 'зайдёте в магазин'}?
                </label>
                <div className="modal-delivery-row">
-                  <input className='modal-delivery__form-input modal-delivery__form-input--date' type="date" name="date" id="date" />
-                  <select name="time" id="time" className='modal-delivery__form-select'>
+                  <input value={deliveryDate} onChange={(e: ChangeEvent<HTMLInputElement>) => setDeliveryDate(e.target.value)} className='modal-delivery__form-input modal-delivery__form-input--date' type="date" name="date" id="date" required />
+                  <select name="time" id="time" value={deliveryTime} onChange={(e: ChangeEvent<HTMLSelectElement>) => setDeliveryTime(e.target.value)} className='modal-delivery__form-select' required>
                      <option value="for 25 mins">за 25 мин.</option>
                      <option value="for 2 hours">с 21:00 до 23:00</option>
                   </select>
                </div>
             </div>
-            <button className='modal-delivery__form-submit' onClick={ (e: MouseEvent<HTMLButtonElement>) => {e.preventDefault(); onModalChange(false)} } type="submit">Сохранить</button>
+            <button className='modal-delivery__form-submit' type="submit">Сохранить</button>
          </form>
          <iframe title='delivery-map' className='modal-delivery__iframe' src="https://yandex.ru/map-widget/v1/?ll=37.570311%2C55.729953&mode=search&poi%5Bpoint%5D=37.483804%2C55.748278&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D1072958798&sll=37.460254%2C55.680326&sspn=0.009194%2C0.003029&text=%D0%BF%D0%B5%D1%80%D0%B5%D0%BA%D1%80%D1%91%D1%81%D1%82%D0%BE%D0%BA&z=12.3" />
-         <button onClick={ (e: MouseEvent<HTMLButtonElement>) => {e.stopPropagation(); onModalChange(false)} } className="modal-delivery__close">x</button>
+         <button onClick={(e: MouseEvent<HTMLButtonElement>) => { e.stopPropagation(); onModalChange(false) }} type='button' className="modal-delivery__close">x</button>
       </div>
    )
 }
