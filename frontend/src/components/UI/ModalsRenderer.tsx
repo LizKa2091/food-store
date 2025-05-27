6fad4a89dcb41e915b7de1e51f9206fae9864610
lizka <lizka4231@gmail.com>
2025-05-28 00:09:08 +0300

    feat: add modalsrenderer component

---
+++ frontend/src/components/UI/ModalsRenderer.tsx
@@ -0,0 +1,18 @@
+import { useModal } from '../../context/ModalContext';
+import ModalDeliveryMethod from '../layout/header/ModalDeliveryMethod';
+import ItemCard from './ItemCard/ItemCard';
+
+export const ModalsRenderer = () => {
+   const { currentModal, closeModal } = useModal();
+
+   return (
+      <>
+         {currentModal === 'delivery' && (
+            <ModalDeliveryMethod onClose={closeModal} />
+         )}
+         {currentModal?.startsWith('product-') && (
+            <ItemCard id={currentModal.replace('product-', '')} onClose={closeModal} />
+         )}
+      </>
+   );
+};
